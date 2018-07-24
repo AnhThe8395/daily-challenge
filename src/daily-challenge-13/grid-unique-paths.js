e410ee87ea7d0ee48980dec6774ed6e4bbeb5940
AnhThe8395 <nguyentheanh839@gmail.com>
2018-07-24 11:18:44 +0700

    update dailly 13

--- src/daily-challenge-13/grid-unique-paths.js
+++ src/daily-challenge-13/grid-unique-paths.js
@@ -11,7 +11,35 @@ const gridUniquePaths = (a, b) => {
     runRoad(0, a - 1 + b - 1, []);
     return result;
 }
-
+const recursionGridUnique = (a, b) => {
+    if (a === 1 || b === 1) return 1;
+    else return recursionGridUnique(a - 1, b) + recursionGridUnique(a, b - 1);
+}
+const gridUniquePaths2 = (a, b) => {
+    const result = recursionGridUnique(a, b)
+    return result;
+}
+const glosbe = a => {
+    if (a === 1) return 1;
+    else return a * glosbe(a - 1);
+}
+const gridUniquePaths3 = (a, b) => {
+    return glosbe(a + b - 2) / (glosbe(a - 1) * (glosbe(b - 1)));
+}
+const gridUniquePaths4 = (a, b) => {
+    let matrixRoad = [];
+    for (let i = 0; i < a; i++) {
+        matrixRoad[i] = [];
+        for (let j = 0; j < b; j++) {
+            if (i === 0 || j === 0) matrixRoad[i][j] = 1;
+            else matrixRoad[i][j] = matrixRoad[i - 1][j] + matrixRoad[i][j - 1];
+        }
+    }
+    return matrixRoad[a - 1][b - 1];
+}
 export {
-    gridUniquePaths
+    gridUniquePaths,
+    gridUniquePaths2,
+    gridUniquePaths3,
+    gridUniquePaths4
 }
--- src/daily-challenge-13/grid-unique-paths.spec.js
+++ src/daily-challenge-13/grid-unique-paths.spec.js
@@ -1,18 +1,23 @@
-import { gridUniquePaths } from './grid-unique-paths';
+import {
+    gridUniquePaths,
+    gridUniquePaths2,
+    gridUniquePaths3,
+    gridUniquePaths4
+} from './grid-unique-paths';
 
 describe('Grid Unique Paths', () => {
     it('should return 1', done => {
-        const rs = gridUniquePaths(2, 2);
+        const rs = gridUniquePaths4(2, 2);
         expect(rs).toBe(2);
         done();
     });
     it('should return 2', done => {
-        const rs = gridUniquePaths(4, 4);
+        const rs = gridUniquePaths4(4, 4);
         expect(rs).toBe(20);
         done();
     });
     it('should return 3', done => {
-        const rs = gridUniquePaths(15, 9);
+        const rs = gridUniquePaths4(15, 9);
         expect(rs).toBe(319770);
         done();
     });
