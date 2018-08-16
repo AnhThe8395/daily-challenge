77b5ce0a4cf0f9d0595a6ae092f174b6de0adda1
AnhThe8395 <nguyentheanh839@gmail.com>
2018-08-16 09:01:48 +0700

    update binary

--- src/daily-challenge-21/add-binary-strings.js
+++ src/daily-challenge-21/add-binary-strings.js
@@ -6,24 +6,8 @@ const addBinaryStrings = (binary1, binary2) => {
         const num1 = Number.parseInt(numBinary1.pop()) ? 1 : 0;
         const num2 = Number.parseInt(numBinary2.pop()) ? 1 : 0;
         const sum = num1 + num2 + memory;
-        switch (sum) {
-            case 1:
-                sumBinary.push(1);
-                memory = 0;
-                break;
-            case 2:
-                sumBinary.push(0);
-                memory = 1;
-                break;
-            case 3:
-                sumBinary.push(1);
-                memory = 1;
-                break;
-            default:
-                sumBinary.push(0);
-                memory = 0;
-                break;
-        }
+        sumBinary.push(sum % 2);
+        memory = Math.floor(sum / 2);
     }
     if (memory) sumBinary.push(memory);
     return sumBinary.reverse().join('');
